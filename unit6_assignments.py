179fb3a088bfc4d375ea18498136972ca69be0ed
SimpleBeat <simplebeat.katulin@gmail.com>
2022-03-19 22:52:43 +0300

    update assignments file
    Couple of solutions: dec to hex converter, and dec color to hex color converter

--- unit6_assignments.py
+++ unit6_assignments.py
@@ -15,3 +15,56 @@ def my_range(m, n):
         m += 1
 
     return range
+
+# Assignment 6.5 - Color Hex Codes
+# Function 1 - Converting decimals in the range from 0 to 255 to HEX codes
+def to_hex(decNum):
+    if decNum < 0 or decNum > 255:
+        print("The number is out of range (0-255)!")
+        return "" # check for valid input range
+    
+    # array of hex digits
+    hexDigits = "0123456789abcdef"
+
+    # calculating each hex digit
+    digit1 = hexDigits[decNum // 16] # integer division
+    digit2 = hexDigits[decNum % 16]  # remainder
+
+    hexNum = ""
+
+    if digit1 == "0":
+        hexNum = digit2 # if the first digit is 0 then get rid of it
+    else:
+        hexNum = digit1 + digit2
+
+    return hexNum
+
+# Function 1 variation for the second assignment
+# Converts decimal number (in the range 0 - 255) to hex
+
+# Difference from the function above is that this function
+# returns hex value with a leading "0", while the one above does not
+
+def doubleDigit_hex(decNum):
+    if decNum < 0 or decNum > 255:
+        print("The number is out of range (0-255)!")
+        return "" # check for valid input range
+    
+    # array of hex digits
+    hexDigits = "0123456789abcdef"
+
+    # calculating each hex digit
+    digit1 = hexDigits[decNum // 16] # integer division
+    digit2 = hexDigits[decNum % 16]  # remainder
+
+    return digit1 + digit2
+
+# Function 2 - Converting an RGB color code from decimal to HEX
+def hex_color(red, green, blue):
+    if red < 0 or green < 0 or blue < 0 or red > 255 or green > 255 or blue > 255:
+        print("Decimal amount(s) out of range (0 - 255)!")
+        return "" # valid input range check
+
+    hexColor = "#" +  doubleDigit_hex(red) + doubleDigit_hex(green) + doubleDigit_hex(blue)
+
+    return hexColor
